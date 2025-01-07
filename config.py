fba7b65e3625980bc00ebdf1f288979036ff155c
Drashti Shah <drashti@inkredo.in>
2025-01-07 11:17:49 +0530

    initial setup

---
+++ config.py
@@ -0,0 +1,5 @@
+# These files should be inside a folder named "data"
+NORMAL_FASTQ_FILE_LIST = ['PA221MH-lib09-P19-Norm_S1_L001_R1_001.fastq.gz','PA221MH-lib09-P19-Norm_S1_L001_R2_001.fastq.gz']
+TUMOR_FASTQ_FILE_LIST = ['PA220KH-lib09-P19-Tumor_S2_L001_R1_001.fastq.gz','PA220KH-lib09-P19-Tumor_S2_L001_R2_001.fastq.gz']
+REFERENCE_CSV_FILE = 'PANCAN_PDAC_100plex_ref.csv'
+METHYLATION_CSV_FILE = 'PupilBioTest_PMP_revA.csv'
